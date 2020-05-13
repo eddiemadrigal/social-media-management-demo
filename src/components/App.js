dfc0c97189a97cb7a16d51a113bb0ceb4766a22a
Eddie Madrigal <edmadrigal@yahoo.com>
2020-05-13 15:22:33 -0700

    @material-ui dashboard integrated

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Dashboard from './Dashboard';
+
 function App() {
   return (
     <div>
-      <h1>Hello you!</h1>
+      <Dashboard />
     </div>
   );
 }
---
+++ src/components/Twitter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Twitter = () => {
+  return (
+    <div>
+      <h2>Twitter Component</h2>
+    </div>
+  )
+}
+
+export default Twitter;
